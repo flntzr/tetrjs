81824a728c091e274bc5ad1363828a4fc654b59f
s0562668 <s0562668@htw-berlin.de>
2018-09-15 18:59:49 +0200

    Add redux devtools browser extension support

--- store/index.js
+++ store/index.js
@@ -5,5 +5,6 @@ import reducers from '../reducers/reducers.js';
 export const store = createStore(
     combineReducers({
         state: reducers
-    })
+    }),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
