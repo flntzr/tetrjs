79047ffa8d430cac32ab3bf7fd25a035090e7869
s0562668 <s0562668@htw-berlin.de>
2018-09-16 14:27:55 +0200

    Add more actions

--- actions/actions.js
+++ actions/actions.js
@@ -1,13 +1,63 @@
 import {ActionCodes} from '../reducers/constants.js'
 
-export const exampleAction = () => (
+export const downAction = () => {
     {
-        type: 'EXAMPLE'
+        type: ActionCodes.DOWN
     }
-)
+}
+
+export const rotateAction = () => {
+    {
+        type: ActionCodes.ROTATE
+        // TODO: target rotation
+    }
+}
+
+export const leftAction = () => {
+    {
+        type: ActionCodes.LEFT
+    }
+}
+
+export const rightAction = () => {
+    {
+        type: ActionCodes.RIGHT
+    }
+}
+export const dropAction = () => {
+    {
+        type: ActionCodes.DROP
+    }
+}
+
+export const spawnAction = () => {
+    {
+        type: ActionCodes.SPAWN
+        // TODO: shape
+    }
+}
+
+export const popAction = () => {
+    {
+        type: ActionCodes.POP
+        // TODO: row indices
+    }
+}
 
 export const gameStartAction = () => (
     {
         type: ActionCodes.GAME_START
+    }
+)
+
+export const gameOverAction = () => {
+    {
+        type: ActionCodes.GAME_OVER
+    }
+}
+
+export const pauseAction = () => (
+    {
+        type: ActionCodes.PAUSE
     }
 )
