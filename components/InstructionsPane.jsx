9b64cb3a7bbf19c3d650a81485a2871f7fcad9b4
s0562668 <s0562668@htw-berlin.de>
2018-09-25 12:03:31 +0200

    Rewrite instructions and style them further

--- components/InstructionsPane.jsx
+++ components/InstructionsPane.jsx
@@ -6,11 +6,56 @@ export default class InstructionsPane extends React.Component {
             <div id="instructions-pane" className="pane">
                 <h1>Instructions</h1>
                 <br/>
-                <p><i className="fas fa-arrows-alt"></i> Use the <b>Arrow Keys</b> to move.</p>
-                <p><i className="fas fa-caret-down"></i> Use the <b>Space Bar</b> to drop.</p>
-                <p><i className="fas fa-pause-circle"></i> Use <b>Escape</b> to pause or press the icon above.</p>
-                <br/>
-                Press <i className="fas fa-play"></i> above to start.
+                <table id="instructions-table">
+                    <tr>
+                        <td>
+                            <i className="fas fa-arrow-up"/>
+                        </td>
+                        <td>
+                            Press the <b>Up Arrow</b> to rotate.
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <i className="fas fa-arrow-left"/> <i className="fas fa-arrow-right"/>
+                        </td>
+                        <td>
+                            Press the <b>Left and Right Arrows</b> to move.
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <i className="fas fa-arrow-down"/>
+                        </td>
+                        <td>
+                            Press the <b>Down Arrow</b> to go down faster.
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <i className="fas fa-caret-down"/>
+                        </td>
+                        <td>
+                            Press the <b>Space Bar</b> to drop.
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <i className="fas fa-pause-circle"/>
+                        </td>
+                        <td>
+                            Press <b>Escape</b> to pause or click on the icon above.
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <i className="fas fa-play"/>
+                        </td>
+                        <td>
+                            Click on the icon above to start the game.
+                        </td>
+                    </tr>
+                </table>
             </div>
         );
     }
